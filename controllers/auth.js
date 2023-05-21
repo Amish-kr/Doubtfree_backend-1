5716a309e2f140045942a1d0bac82f8c72776d02
dishu001 <2020eeb1167@iitrpr.ac.in>
2023-05-21 19:56:54 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,55 +3,74 @@ import bcrypt from "bcryptjs";
 
 import users from "../models/auth.js";
 
-export const signup = async (req, res) => {
-  const { Nickname, email, password, residence, number, donation, website } =
+export const signup = async(req, res) => {
+    const { Nickname, email, password, residence, number, donation, website } =
     req.body;
-  try {
-    const existinguser = await users.findOne({ email });
-    if (existinguser) {
-      return res.status(404).json({ message: "User already Exist." });
+    if (!Nickname || !email | !password | !residence | !number | !donation | !website) {
+        return res.status(400).json({
+            "error": "Empty fields",
+            "message": {
+                "Nickname": "This field is required",
+                "email": "This field is required",
+                "password": "This field is required",
+                "residence": "This field is required",
+                "number": "This field is required",
+                "donation": "This field is required",
+                "website": "This field is required"
+            }
+        });
     }
+    try {
+        const existinguser = await users.findOne({ email });
+        if (existinguser) {
+            return res.status(404).json({ message: "User already Exist." });
+        }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = await users.create({
-      Nickname,
-      email,
-      password: hashedPassword,
-      residence,
-      number,
-      donation,
-      website,
-    });
-    const token = jwt.sign(
-      { email: newUser.email, id: newUser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({ result: newUser, token });
-  } catch (error) {
-    res.status(500).json("Something went worng...");
-  }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await users.create({
+            Nickname,
+            email,
+            password: hashedPassword,
+            residence,
+            number,
+            donation,
+            website,
+        });
+        const token = jwt.sign({ email: newUser.email, id: newUser._id },
+            process.env.JWT_SECRET, { expiresIn: "1h" }
+        );
+        res.status(200).json({ result: newUser, token });
+    } catch (error) {
+        res.status(500).json("Something went worng...");
+    }
 };
 
-export const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const existinguser = await users.findOne({ email });
-    if (!existinguser) {
-      return res.status(404).json({ message: "User don't Exist." });
+export const login = async(req, res) => {
+    const { email, password } = req.body;
+    if (!email | !password) {
+        return res.status(400).json({
+            "error": "Empty fields",
+            "message": {
+                "email": "This field is required",
+                "password": "This field is required"
+            }
+        });
     }
+    try {
+        const existinguser = await users.findOne({ email });
+        if (!existinguser) {
+            return res.status(404).json({ message: "User don't Exist." });
+        }
 
-    const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
-    if (!isPasswordCrt) {
-      return res.status(400).json({ message: "Invalid credentials" });
+        const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
+        if (!isPasswordCrt) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({ email: existinguser.email, id: existinguser._id },
+            process.env.JWT_SECRET, { expiresIn: "1h" }
+        );
+        res.status(200).json({ result: existinguser, token });
+    } catch (error) {
+        res.status(500).json("Something went worng...");
     }
-    const token = jwt.sign(
-      { email: existinguser.email, id: existinguser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({ result: existinguser, token });
-  } catch (error) {
-    res.status(500).json("Something went worng...");
-  }
 };
--- models/auth.js
+++ models/auth.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-  Nickname: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  residence: { type: String, required: true },
-  number: { type: String, required: true },
-  donation: { type: number, required: true },
-  website: { type: String, required: true },
+    Nickname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    residence: { type: String, required: true },
+    number: { type: String, required: true },
+    donation: { type: Number, required: true },
+    website: { type: String, required: true },
 });
 
 export default mongoose.model("User", userSchema);
